c610e81bef2f3387401d40a7c3958f65deb7601e
Amélie Bocquet <amelie.bocquet@isen.yncrea.fr>
2019-05-17 17:06:45 +0200

    c'est bon le cassoulet

--- index.php
+++ index.php
@@ -2,9 +2,9 @@
 <html lang="fr" dir="ltr">
   <head>
     <meta charset="utf-8">
-    <title></title>
+    <title>Projet S2</title>
   </head>
   <body>
-
+		<img src="https://upload.wikimedia.org/wikipedia/commons/9/9b/Bowl_of_cassoulet.jpg" />
   </body>
 </html>
