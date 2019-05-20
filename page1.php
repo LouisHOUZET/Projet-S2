4c988c25a22aa502ad726ed72245acdc521edeb7
Lacomblez <lea.lacomblez@isen.yncrea.fr>
2019-05-20 10:03:51 +0200

    Page de Connexion

---
+++ page1.php
@@ -0,0 +1,21 @@
+<html>
+	<head>
+		<title>PAGE DE CONNEXION</title>
+		<meta charset="utf-8">
+		</head>
+		<body>
+			<div align="center">
+				<h3>Connexion</h3>
+				<br /><br />
+				<form method="POST" action="">
+					<table>
+					<td>
+						<label for "Login">Login :</label>
+					</td>
+					<td>
+						<input type="text" placeholder="Votre Login" id="pseudo" name="pseudo"/>
+					</td>
+					
+				</form>
+		</body>
+</html>
